39360024d23bd135ef0b4597bcda49fdfa51499c
lm2612 <lauraman@stanford.edu>
2025-04-01 22:36:04 +0100

    added python and pyro ml models

---
+++ ml_params/BayesianModels.py
@@ -0,0 +1,58 @@
+import numpy as np
+import torch
+
+import pyro
+import pyro.distributions as dist
+from pyro.nn import PyroModule, PyroSample
+
+class BayesianLinearRegression(PyroModule):
+    """Bayesian linear regression"""
+    def __init__(self, n_features=1, n_targets=1):
+        super().__init__()
+        self.linear = PyroModule[torch.nn.Linear](n_features, n_targets)
+        self.linear.weight = PyroSample(dist.Normal(0., 1.).expand([n_targets, n_features]).to_event(2))
+        self.linear.bias = PyroSample(dist.Normal(0., 10.).expand([n_targets]).to_event(1))
+
+    def forward(self, x, y=None):
+        sigma = pyro.sample("sigma", dist.Uniform(0., 10.))
+        mean = self.linear(x) 
+        with pyro.plate("data", x.shape[0]):
+            obs = pyro.sample("obs", dist.Normal(mean, sigma).to_event(1), obs=y)
+        return mean
+
+class BayesianNN(PyroModule):
+    """Bayesian neural network with two hidden layers"""
+    def __init__(self, n_features=1, n_targets=1, n_hidden=[32, 32]):
+        super().__init__()
+        self.n_features = n_features
+        self.n_targets = n_targets
+        self.n_hidden = n_hidden
+
+        # 2 layers NN
+        self.linear_1 = PyroModule[torch.nn.Linear](n_features, n_hidden[0])
+        self.linear_1.weight = PyroSample(dist.Normal(0., 1.).expand([n_hidden[0], n_features]).to_event(2))
+        self.linear_1.bias = PyroSample(dist.Normal(0., 10.).expand([n_hidden[0]]).to_event(1))
+
+        self.linear_2 = PyroModule[torch.nn.Linear](n_hidden[0], n_hidden[1])
+        self.linear_2.weight = PyroSample(dist.Normal(0., 1.).expand([n_hidden[1], n_hidden[0]]).to_event(2))
+        self.linear_2.bias = PyroSample(dist.Normal(0., 10.).expand([n_hidden[1]]).to_event(1))
+
+        self.linear_3 = PyroModule[torch.nn.Linear](n_hidden[1], n_targets)
+        self.linear_3.weight = PyroSample(dist.Normal(0., 1.).expand([n_targets, n_hidden[1]]).to_event(2))
+        self.linear_3.bias = PyroSample(dist.Normal(0., 10.).expand([n_targets]).to_event(1))
+
+        self.activation_function = torch.nn.ReLU()
+
+
+    def forward(self, x, y=None):
+        sigma = pyro.sample("sigma", dist.Uniform(0., 10.))
+
+        mean = self.linear_1(x)
+        mean = self.activation_function(mean)
+        mean = self.linear_2(mean)
+        mean = self.activation_function(mean)
+        mean = self.linear_3(mean)
+
+        with pyro.plate("data", x.shape[0]):
+            obs = pyro.sample("obs", dist.Normal(mean, sigma).to_event(1), obs=y)
+        return mean
---
+++ ml_params/TorchModels.py
@@ -0,0 +1,33 @@
+import numpy as np
+import torch
+
+class LinearRegression(torch.nn.Module):
+    """Linear regression"""
+    def __init__(self, n_features=1, n_targets=1):
+        super().__init__()
+        self.linear = torch.nn.Linear(n_features, n_targets)
+    
+    def forward(self, X):
+        return self.linear(X)
+
+class NN(torch.nn.Module):
+    """Simple feedforward neural network with two hidden layers"""
+    def __init__(self, n_features=1, n_targets=1, n_hidden = [32, 32]):
+        super().__init__()
+        self.n_features = n_features
+        self.n_targets = n_targets
+
+        self.layer_input = torch.nn.Linear(self.n_features, n_hidden[0])         # Input layer: n_features -> 16
+        self.layer_hidden1 = torch.nn.Linear(n_hidden[0], n_hidden[1])           # Hidden layer 1: 16 -> 16
+        self.layer_hidden2 = torch.nn.Linear(n_hidden[1], self.n_targets)        # Hidden layer 2: 16 -> n_targets
+
+        self.activation_function = torch.nn.ReLU()
+
+    def forward(self, X):
+        output = self.layer_input(X)
+        output = self.activation_function(output)
+        output = self.layer_hidden1(output)
+        output = self.activation_function(output)
+        output = self.layer_hidden2(output)
+        return output
+    

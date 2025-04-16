74be441db736e4dcba438f75f994fde717e2cb4d
lm2612 <lauraman@stanford.edu>
2025-04-16 09:43:17 +0100

    add dropout

--- ml_models/TorchModels.py
+++ ml_models/TorchModels.py
@@ -30,4 +30,28 @@ class NN(torch.nn.Module):
         output = self.activation_function(output)
         output = self.layer_hidden2(output)
         return output
-    
+
+
+class NNDropout(torch.nn.Module):
+    """Simple feedforward neural network with two hidden layers"""
+    def __init__(self, n_features=1, n_targets=1, n_hidden = [32, 32], dropout_rate=0.):
+        super().__init__()
+        self.n_features = n_features
+        self.n_targets = n_targets
+
+        self.layer_input = torch.nn.Linear(self.n_features, n_hidden[0])         # Input layer: n_features -> 16
+        self.layer_hidden1 = torch.nn.Linear(n_hidden[0], n_hidden[1])           # Hidden layer 1: 16 -> 16
+        self.layer_hidden2 = torch.nn.Linear(n_hidden[1], self.n_targets)        # Hidden layer 2: 16 -> n_targets
+        self.dropout = torch.nn.Dropout(dropout_rate)
+
+        self.activation_function = torch.nn.ReLU()
+
+    def forward(self, X):
+        output = self.layer_input(X)
+        output = self.activation_function(output)
+        output = self.dropout(output)
+        output = self.layer_hidden1(output)
+        output = self.activation_function(output)
+        output = self.dropout(output)
+        output = self.layer_hidden2(output)
+        return output

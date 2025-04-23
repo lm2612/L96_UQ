eee31b8aedcf4bfcd91685ef91d94e2f766f708d
lm2612 <lauraman@stanford.edu>
2025-04-23 16:41:20 +0100

    main functions

--- scripts/online_test.py
+++ scripts/online_test.py
@@ -13,193 +13,195 @@ from ml_models.BayesianModels import FixedParamNN
 
 from L96.L96_model import L96OneLayerParam
 
-# Get paths
-# Define dimensions of system (fixed)
-K = 8   
-J = 32  
+def test(params, test_params, model_name):
+    K, J, h, F, c, b = params['K'], params['J'], params['h'], params['F'], params['c'], params['b']
+    dt, dt_f = params['dt'], params['dt_f']
+    runtype = test_params['runtype']
+    n_ens = test_params['runtype']
 
-# Define the "true" parameters
-h = 1
-F = 20  # 8
-c = 10
-b = 10
-
-# Define time-stepping, random seed
-dt = 0.001
-dt_f = dt * 5
-seed = 123
-np.random.seed(seed)
-
-N_train = 50
-model_name = f"BayesianNN_2layer_N{N_train}"      # Choose LinearRegression or NN  or OneLayer
-#model_name = f"BayesianLinearRegression_N{N_train}"
-#model_name = f"BayesianNN_2layer_N{N_train}"
-#model_name = f"BayesianLinearRegression_N{N_train}"
-runtype = "both"    # epistemic, aleatoric or None
-n_ens = 50               # number of times to run for (for deterministic this will be 1) (quite slow when running large ensembles eg 50)
-# Set up directory
-data_path = f'./data/K{K}_J{J}_h{h}_c{c}_b{b}_F{F}'
-save_model_path = f'{data_path}/{model_name}/'
-if not os.path.exists(save_model_path):
-    os.makedirs(save_model_path)
-print(save_model_path)
+    # Set up directory
+    data_path = f'./data/K{K}_J{J}_h{h}_c{c}_b{b}_F{F}'
+    save_model_path = f'{data_path}/{model_name}/'
+    if not os.path.exists(save_model_path):
+        os.makedirs(save_model_path)
+    print(save_model_path)
 
 
-# Load ml param model: This depends on the type of the model
-if "Bayesian" in model_name:
-    print(f"Running Bayesian NN - a stochastic parameterisation: {model_name} using {runtype} uncertainty, {n_ens} ensemble members")
-    output_dicts = torch.load(f"{save_model_path}/model_best.pt")
-    pyro.get_param_store().load(f"{save_model_path}/pyro_params.pt")
+    # Load ml param model: This depends on the type of the model
+    if "Bayesian" in model_name:
+        print(f"Running Bayesian NN - a stochastic parameterisation: {model_name} using {runtype} uncertainty, {n_ens} ensemble members")
+        output_dicts = torch.load(f"{save_model_path}/model_best.pt")
+        pyro.get_param_store().load(f"{save_model_path}/pyro_params.pt")
 
-    pyro_model = output_dicts["model"]
-    guide = output_dicts["guide"]
-    # Stochastic parameterisation
-    if runtype == "epistemic":
-        return_site = "_RETURN" 
-        predictive = Predictive(pyro_model, guide=guide, num_samples=1,
-                    return_sites=(return_site,))
-    elif runtype == "both":
-        return_site = "obs"
-        predictive = Predictive(pyro_model, guide=guide, num_samples=1,
-                    return_sites=(return_site,))
-    elif runtype == "aleatoric":
-        return_site = "obs"
-        fixed_param_NN = FixedParamNN(pyro_model, guide)
-        fixed_param_NN.eval()
-        predictive = Predictive(fixed_param_NN, guide=guide, num_samples=1,
-                    return_sites=(return_site,))
-    elif runtype == "deterministic":
-        if n_ens != 1:
-            warnings.warn(f"runtype not valid for deterministic run ({model_name}). You set n_ens={n_ens}. This will be ignored and only one member run.")
-            n_ens = 1
-        return_site = "_RETURN"
-        fixed_param_NN = FixedParamNN(pyro_model, guide)
-        fixed_param_NN.eval()
-        predictive = Predictive(fixed_param_NN, guide=guide, num_samples=1,
-                    return_sites=(return_site,))
-    elif runtype == "mean":
-        # Todo
-        return_site = "obs"
-    else:
-        raise ValueError(f"{runtype} unknown, must be epistemic, aleatoric or mean.")
-    def param_func(x):
-        out = predictive(x.unsqueeze(-1))[return_site]
-        return out.squeeze()
-
-    save_model_path = f'{save_model_path}/{runtype}_'
-elif "Aleatoric" in model_name:
-    print(f"Stochastic run: {model_name}")
-    if runtype == "epistemic":
-        raise ValueError(f"Must be run either in aleatoric or determinstic mode.")
-    
-    output_dicts = torch.load(f"{save_model_path}/model_best.pt")
-    ml_model = output_dicts["model"]
-    ml_model.eval()
-
-    if runtype == "deterministic":
-        # Initialize param_func
+        pyro_model = output_dicts["model"]
+        guide = output_dicts["guide"]
+        # Stochastic parameterisation
+        if runtype == "epistemic":
+            return_site = "_RETURN" 
+            predictive = Predictive(pyro_model, guide=guide, num_samples=1,
+                        return_sites=(return_site,))
+        elif runtype == "both":
+            return_site = "obs"
+            predictive = Predictive(pyro_model, guide=guide, num_samples=1,
+                        return_sites=(return_site,))
+        elif runtype == "aleatoric":
+            return_site = "obs"
+            fixed_param_NN = FixedParamNN(pyro_model, guide)
+            fixed_param_NN.eval()
+            predictive = Predictive(fixed_param_NN, guide=guide, num_samples=1,
+                        return_sites=(return_site,))
+        elif runtype == "deterministic":
+            if n_ens != 1:
+                warnings.warn(f"runtype not valid for deterministic run ({model_name}). You set n_ens={n_ens}. This will be ignored and only one member run.")
+                n_ens = 1
+            return_site = "_RETURN"
+            fixed_param_NN = FixedParamNN(pyro_model, guide)
+            fixed_param_NN.eval()
+            predictive = Predictive(fixed_param_NN, guide=guide, num_samples=1,
+                        return_sites=(return_site,))
+        elif runtype == "mean":
+            # Todo
+            return_site = "obs"
+        else:
+            raise ValueError(f"{runtype} unknown, must be epistemic, aleatoric or mean.")
         def param_func(x):
-            with torch.no_grad():
-                pred = ml_model(x.unsqueeze(-1))
-            # Split into mean and variance
-            mean, std = pred.chunk(2, dim=-1)
-            return mean.squeeze()
+            out = predictive(x.unsqueeze(-1))[return_site]
+            return out.squeeze()
+
         save_model_path = f'{save_model_path}/{runtype}_'
-
-    else:
-        # Initialize param_func
-        def param_func(x):
-            with torch.no_grad():
-                pred = ml_model(x.unsqueeze(-1))
-            # Split into mean and variance
-            mean, std = pred.chunk(2, dim=-1)
-            out = np.random.normal(loc=mean.squeeze(), scale=std.squeeze())
-            return  out
-elif "Dropout" in model_name:
-   
-    print(f"Dropout run: {model_name}")
-    output_dicts = torch.load(f"{save_model_path}/model_best.pt")
-    ml_model = output_dicts["model"]
-    if runtype == "epistemic":
-        # Use training mode rather than eval mode to add stochasticity!
-        ml_model.train()
-    elif runtype == "deterministic":
-        ml_model.eval()
-        n_ens = 1
-        save_model_path = f'{save_model_path}/{runtype}_'
-    else:
-        raise ValueError(f"Must be run either in epistemic or deterministic mode.")
-
-
-    # Initialize param_func
-    def param_func(x):
-        with torch.no_grad():
-            out = ml_model(x.unsqueeze(-1))
-        return  out.squeeze()
-else:
-    print(f"Deterministic run: {model_name}")
-    if runtype != "":
-        warnings.warn(f"runtype not valid for deterministic run. You set runtype={runtype}. This will be ignored.")
-        runtype = ""
-    if n_ens != 1:
-        warnings.warn(f"runtype not valid for deterministic run ({model_name}). You set n_ens={n_ens}. This will be ignored and only one member run.")
-        n_ens = 1
-
-    if model_name == "OneLayer":
-        print("Running single layer model with no parameterisation")
-        # run with zero parameterisation
-        # Initialize param_func
-        def param_func(x):
-            return  np.zeros_like(x)
-    else:
+    elif "Aleatoric" in model_name:
+        print(f"Stochastic run: {model_name}")
+        if runtype == "epistemic":
+            raise ValueError(f"Must be run either in aleatoric or determinstic mode.")
+        
         output_dicts = torch.load(f"{save_model_path}/model_best.pt")
         ml_model = output_dicts["model"]
         ml_model.eval()
+
+        if runtype == "deterministic":
+            # Initialize param_func
+            def param_func(x):
+                with torch.no_grad():
+                    pred = ml_model(x.unsqueeze(-1))
+                # Split into mean and variance
+                mean, std = pred.chunk(2, dim=-1)
+                return mean.squeeze()
+            save_model_path = f'{save_model_path}/{runtype}_'
+
+        else:
+            # Initialize param_func
+            def param_func(x):
+                with torch.no_grad():
+                    pred = ml_model(x.unsqueeze(-1))
+                # Split into mean and variance
+                mean, std = pred.chunk(2, dim=-1)
+                out = np.random.normal(loc=mean.squeeze(), scale=std.squeeze())
+                return  out
+    elif "Dropout" in model_name:
+    
+        print(f"Dropout run: {model_name}")
+        output_dicts = torch.load(f"{save_model_path}/model_best.pt")
+        ml_model = output_dicts["model"]
+        if runtype == "epistemic":
+            # Use training mode rather than eval mode to add stochasticity!
+            ml_model.train()
+        elif runtype == "deterministic":
+            ml_model.eval()
+            n_ens = 1
+            save_model_path = f'{save_model_path}/{runtype}_'
+        else:
+            raise ValueError(f"Must be run either in epistemic or deterministic mode.")
+
 
         # Initialize param_func
         def param_func(x):
             with torch.no_grad():
                 out = ml_model(x.unsqueeze(-1))
             return  out.squeeze()
+    else:
+        print(f"Deterministic run: {model_name}")
+        if runtype != "":
+            warnings.warn(f"runtype not valid for deterministic run. You set runtype={runtype}. This will be ignored.")
+            runtype = ""
+        if n_ens != 1:
+            warnings.warn(f"runtype not valid for deterministic run ({model_name}). You set n_ens={n_ens}. This will be ignored and only one member run.")
+            n_ens = 1
 
-# Load truth data
-X_truth = np.load(f"{data_path}/truth/X_dtf.npy")
-# Select initial conditions, separated by intervals of 10MTU 
-T = 10
-sep = int(T/dt_f)
-print(f"Initial conditions separated by {sep} time units")
-X_init_conds = X_truth[::sep]
-N_init = X_init_conds.shape[0]
-nt_total = X_truth.shape[0]
+        if model_name == "OneLayer":
+            print("Running single layer model with no parameterisation")
+            # run with zero parameterisation
+            # Initialize param_func
+            def param_func(x):
+                return  np.zeros_like(x)
+        else:
+            output_dicts = torch.load(f"{save_model_path}/model_best.pt")
+            ml_model = output_dicts["model"]
+            ml_model.eval()
 
-# Check 
-nt = int(T/dt_f)
-assert(N_init * nt == nt_total)
-print(f"Running model for {N_init} initial conditions, for T={T}MTU / {nt} timesteps). Total timesteps={nt_total}. ")
+            # Initialize param_func
+            def param_func(x):
+                with torch.no_grad():
+                    out = ml_model(x.unsqueeze(-1))
+                return  out.squeeze()
 
-# Run each model for 10MTU
-X_all = np.zeros((n_ens, N_init * nt, K))
-U_all = np.zeros((n_ens, N_init * nt, K))
-t=0
+    # Load truth data
+    X_truth = np.load(f"{data_path}/truth/X_dtf.npy")
+    # Select initial conditions, separated by intervals of 10MTU 
+    T = 10
+    sep = int(T/dt_f)
+    print(f"Initial conditions separated by {sep} time units")
+    X_init_conds = X_truth[::sep]
+    N_init = X_init_conds.shape[0]
+    nt_total = X_truth.shape[0]
 
-for i in range(N_init):
-    print(f"Initial condition {i}")
-    # Repeat for n_ens ensemble members (n_ens = 1 if deterministic)
-    for n in range(n_ens):
-        # Initialize model
-        l96_model = L96OneLayerParam(X_0=X_init_conds[i], 
-                                    param_func=param_func, 
-                                    dt=dt_f, 
-                                    F=F)
+    # Check 
+    nt = int(T/dt_f)
+    assert(N_init * nt == nt_total)
+    print(f"Running model for {N_init} initial conditions, for T={T}MTU / {nt} timesteps). Total timesteps={nt_total}. ")
 
-        # Run model
-        X, U, time = l96_model.iterate(T)
-        X_all[n, i*nt:(i+1)*nt, :] = X
-        U_all[n, i*nt:(i+1)*nt, :] = U
+    # Run each model for 10MTU
+    X_all = np.zeros((n_ens, N_init * nt, K))
+    U_all = np.zeros((n_ens, N_init * nt, K))
+    t=0
+
+    for i in range(N_init):
+        print(f"Initial condition {i}")
+        # Repeat for n_ens ensemble members (n_ens = 1 if deterministic)
+        for n in range(n_ens):
+            # Initialize model
+            l96_model = L96OneLayerParam(X_0=X_init_conds[i], 
+                                        param_func=param_func, 
+                                        dt=dt_f, 
+                                        F=F)
+
+            # Run model
+            X, U, time = l96_model.iterate(T)
+            X_all[n, i*nt:(i+1)*nt, :] = X
+            U_all[n, i*nt:(i+1)*nt, :] = U
 
 
-# Save results
-np.save(f"{save_model_path}X_dtf.npy", X_all)
-np.save(f"{save_model_path}U_dtf.npy", U_all)
+    # Save results
+    np.save(f"{save_model_path}X_dtf.npy", X_all)
+    np.save(f"{save_model_path}U_dtf.npy", U_all)
 
-print(f"Done. Saved to {save_model_path}")
+    print(f"Done. Saved to {save_model_path}")
+
+
+if __name__ == "__main__":
+    params ={
+        'F': 20,
+        'c': 10,
+        'b': 10,
+        'h': 1,
+        'J': 32,
+        'K': 8,
+        'dt': 0.001,
+        'dt_f': 0.005,
+    }
+    test_params = {'runtype': None, 
+                   'n_ens': 1 }
+    N_train = 100
+    seeds = range(100, 150)
+    for seed in seeds:
+        model_name =  f"NN_2layer_N{N_train}_seed{seed}"      # Choose LinearRegression or NN 
+        test(params, test_params, model_name)
--- scripts/train.py
+++ scripts/train.py
@@ -6,149 +6,151 @@ import torch
 
 from ml_models.TorchModels import LinearRegression, NN, NNDropout
 
-# Get paths
-# Define dimensions of system (fixed)
-K = 8   
-J = 32  
 
-# Define the "true" parameters
-h = 1
-F = 20  # 8
-c = 10
-b = 10
+def train(params, training_params, model_name, model):
+    K, J, h, F, c, b = params['K'], params['J'], params['h'], params['F'], params['c'], params['b']
+    dt, dt_f = params['dt'], params['dt_f']
+    N_train = training_params['N_train']
 
-# Define time-stepping, random seed
-dt = 0.001
-dt_f = 0.05
-seed = 123
-np.random.seed(seed)
+    # Set up directory
+    data_path = f'./data/K{K}_J{J}_h{h}_c{c}_b{b}_F{F}'
+    save_model_path = f'{data_path}/{model_name}/'
+    if not os.path.exists(save_model_path):
+        os.makedirs(save_model_path)
+    print(save_model_path)
 
-N_train = 50
-dr = 0.9
-model_name =  f"Bayesian_2layer_N{N_train}"      # Choose LinearRegression or NN 
-model = NNDropout(1, 1, [32, 32]) #, dropout_rate=0.5)
-total_params = sum(p.numel() for p in model.parameters())
+    # Get data
+    X = np.load(f'{data_path}/X_train_dtf.npy')
+    U = np.load(f'{data_path}/U_train_dtf.npy')
+    print(f'Data loaded from {data_path}')
 
-print("TOTAL PARAMS: ", total_params)
+    # Subsample to remove correlations
+    subsample = 1000 # (1 Time Units)
+    X = X[::subsample]
+    U = U[::subsample]
 
-# Set up directory
-data_path = f'./data/K{K}_J{J}_h{h}_c{c}_b{b}_F{F}'
-save_model_path = f'{data_path}/{model_name}/'
-if not os.path.exists(save_model_path):
-    os.makedirs(save_model_path)
-print(save_model_path)
+    N = X.shape[0]
+    N_train = N_train #int(0.60 * N)
+    N_val = max(N - N_train, 0)   # Use remainder for validation
 
-# Get data
-X = np.load(f'{data_path}/X_train_dtf.npy')
-U = np.load(f'{data_path}/U_train_dtf.npy')
-print(f'Data loaded from {data_path}')
+    features = np.ravel(X[:N_train])   
+    targets = np.ravel(U[:N_train])   
 
-# Subsample to remove correlations
-subsample = 1000 # (1 Time Units)
-X = X[::subsample]
-U = U[::subsample]
+    features_val = np.ravel(X[N_train:N_train+N_val])   
+    targets_val = np.ravel(U[N_train:N_train+N_val])    
 
-N = X.shape[0]
-N_train = N_train #int(0.60 * N)
-N_val = max(N - N_train, 0)   # Use remainder for validation
+    print(features.shape, targets.shape, features_val.shape, targets_val.shape)
 
-features = np.ravel(X[:N_train])   
-targets = np.ravel(U[:N_train])   
+    X_torch = torch.tensor(features, dtype=torch.float32).reshape((-1, 1))
+    Y_torch = torch.tensor(targets, dtype=torch.float32).reshape((-1, 1))
 
-features_val = np.ravel(X[N_train:N_train+N_val])   
-targets_val = np.ravel(U[N_train:N_train+N_val])    
+    X_val = torch.tensor(features_val, dtype=torch.float32).reshape((-1, 1))
+    Y_val = torch.tensor(targets_val, dtype=torch.float32).reshape((-1, 1))
 
-print(features.shape, targets.shape, features_val.shape, targets_val.shape)
+    # Optimisation settings
+    optimiser = torch.optim.Adam(params = model.parameters(), lr=1e-2)
+    loss_function = torch.nn.MSELoss()
 
-X_torch = torch.tensor(features, dtype=torch.float32).reshape((-1, 1))
-Y_torch = torch.tensor(targets, dtype=torch.float32).reshape((-1, 1))
+    num_iterations=500
+    losses = []
+    losses_val = []
+    min_loss = 1E8
 
-X_val = torch.tensor(features_val, dtype=torch.float32).reshape((-1, 1))
-Y_val = torch.tensor(targets_val, dtype=torch.float32).reshape((-1, 1))
+    for iteration in range(num_iterations):
+        model.train()
+        optimiser.zero_grad()
+        pred = model(X_torch)
+        loss = loss_function(pred, Y_torch)
+        loss.backward()
 
-# Optimisation settings
-optimiser = torch.optim.Adam(params = model.parameters(), lr=1e-2)
-loss_function = torch.nn.MSELoss()
+        losses.append(loss.item())
 
-num_iterations=500
-losses = []
-losses_val = []
-min_loss = 1E8
+        # Update optimiser
+        optimiser.step()
 
-for iteration in range(num_iterations):
-    model.train()
-    optimiser.zero_grad()
-    pred = model(X_torch)
-    loss = loss_function(pred, Y_torch)
-    loss.backward()
+        # validation
+        model.eval()
+        pred = model(X_val)
+        loss = loss_function(pred, Y_val)
+        losses_val.append(loss.item())
 
-    losses.append(loss.item())
+        if loss < min_loss:
+            # Save checkpoint
+            output_dicts = {
+                "iteration": iteration,
+                "val_loss": losses_val[-1],
+                "train_loss": losses[-1],
+                "model": model}
 
-    # Update optimiser
-    optimiser.step()
+            torch.save(output_dicts, f"{save_model_path}/model_best.pt")
+            min_loss = loss
 
-    # validation
+    print("Done training")
+
+    # Save results
+    output_dicts = {
+        "iteration": iteration,
+        "val_loss": losses_val[-1],
+        "train_loss": losses[-1],
+        "np_rng_state": np.random.get_state(),
+        "torch_rng_state": torch.random.get_rng_state(),
+        "model":model}
+
+    torch.save(output_dicts, f"{save_model_path}/model.pt")
+    print("Model saved to ", save_model_path)
+
+
+    # Plot and save losses
+    plt.clf()
+    figure, ax = plt.subplots(1)
+    plt.semilogy(losses)
+    #plt.semilogy(losses_val, alpha=0.5)
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.savefig(f"{save_model_path}/losses.png")
+
+    # Plot and save result
     model.eval()
-    pred = model(X_val)
-    loss = loss_function(pred, Y_val)
-    losses_val.append(loss.item())
+    plt.clf()
+    figure, ax = plt.subplots(1)
+    X_domain = torch.linspace(-15, 20., 100).unsqueeze(-1)
+    pred = model(X_domain).detach()
 
-    if loss < min_loss:
-        # Save checkpoint
-        output_dicts = {
-            "iteration": iteration,
-            "val_loss": losses_val[-1],
-            "train_loss": losses[-1],
-            "model": model}
+    # Plot
+    plt.scatter(X_torch.flatten()[::], Y_torch.flatten()[::], color="k", alpha=0.2)
+    plt.axis(ymin=-15., ymax=20.,xmin=-15., xmax=20.)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.xlabel("$X$", fontsize=18)
+    plt.ylabel("$U$", fontsize=18)
+    plt.tight_layout()
+    plt.savefig(f"{save_model_path}/data.png")
+    plt.plot(X_domain.squeeze(), pred.squeeze(), color="r", linewidth=2)
 
-        torch.save(output_dicts, f"{save_model_path}/model_best.pt")
-        min_loss = loss
+    plt.xlabel("Parameterisation input")
+    plt.ylabel("Parameterisation output")
+    plt.title("2-layer NN")
+    plt.savefig(f"{save_model_path}/input_outputs_NN.png")
+    print("Plots done")
 
-print("Done training")
-
-# Save results
-output_dicts = {
-    "iteration": iteration,
-    "val_loss": losses_val[-1],
-    "train_loss": losses[-1],
-    "np_rng_state": np.random.get_state(),
-    "torch_rng_state": torch.random.get_rng_state(),
-    "model":model}
-
-torch.save(output_dicts, f"{save_model_path}/model.pt")
-print("Model saved to ", save_model_path)
-
-
-# Plot and save losses
-plt.clf()
-figure, ax = plt.subplots(1)
-plt.semilogy(losses)
-#plt.semilogy(losses_val, alpha=0.5)
-plt.xlabel("Iterations")
-plt.ylabel("Loss")
-plt.savefig(f"{save_model_path}/losses.png")
-
-# Plot and save result
-model.eval()
-plt.clf()
-figure, ax = plt.subplots(1)
-X_domain = torch.linspace(-15, 20., 100).unsqueeze(-1)
-pred = model(X_domain).detach()
-
-# Plot
-plt.scatter(X_torch.flatten()[::], Y_torch.flatten()[::], color="k", alpha=0.2)
-plt.axis(ymin=-15., ymax=20.,xmin=-15., xmax=20.)
-plt.xticks(fontsize=18)
-plt.yticks(fontsize=18)
-plt.xlabel("$X$", fontsize=18)
-plt.ylabel("$U$", fontsize=18)
-plt.tight_layout()
-plt.savefig(f"{save_model_path}/data.png")
-plt.plot(X_domain.squeeze(), pred.squeeze(), color="r", linewidth=2)
-
-plt.xlabel("Parameterisation input")
-plt.ylabel("Parameterisation output")
-plt.title("2-layer NN")
-plt.savefig(f"{save_model_path}/input_outputs_NN.png")
-print("Plots done")
-
+if __name__ == "__main__":
+    params ={
+        'F': 20,
+        'c': 10,
+        'b': 10,
+        'h': 1,
+        'J': 32,
+        'K': 8,
+        'dt': 0.001,
+        'dt_f': 0.005,
+    }
+    training_params = {'N_train': 100}
+    N_train = training_params['N_train']
+    seeds = range(100, 150)
+    for seed in seeds:
+        model_name =  f"NN_2layer_N{N_train}_seed{seed}"      # Choose LinearRegression or NN 
+        np.random.seed(seed)
+        model = NN(1, 1, [32, 32]) #, dropout_rate=0.5)
+        total_params = sum(p.numel() for p in model.parameters())
+        print("TOTAL PARAMS: ", total_params)
+        train(params, training_params, model_name, model)

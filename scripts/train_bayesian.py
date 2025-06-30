194eec59fd66cd367cdb2b9c62d388e0aebd861b
lm2612 <lauraman@stanford.edu>
2025-06-30 11:30:04 +0100

    added function and main for example script

--- scripts/train_bayesian.py
+++ scripts/train_bayesian.py
@@ -14,164 +14,178 @@ from pyro.optim import Adam
 from ml_models.BayesianModels import BayesianLinearRegression, BayesianNN
 from utils.summary_stats import summary_stats
 
-# Get paths
-# Define dimensions of system (fixed)
-K = 8   
-J = 32  
 
-# Define the "true" parameters
-h = 1
-F = 20  # 8
-c = 10
-b = 10
+def bayesian_train(params, training_params, model_name, model, guide):
+    K, J, h, F, c, b = params['K'], params['J'], params['h'], params['F'], params['c'], params['b']
+    dt, dt_f = params['dt'], params['dt_f']
+    N_train = training_params['N_train']
+    batch_size = training_params['batch_size']
 
-# Define time-stepping, random seed
-dt = 0.001
-dt_f = 0.05
-seed = 123
-np.random.seed(seed)
+    
+    # Set up directory
+    data_path = f'./data/K{K}_J{J}_h{h}_c{c}_b{b}_F{F}'
+    save_model_path = f'{data_path}/{model_name}/'
+    if not os.path.exists(save_model_path):
+        os.makedirs(save_model_path)
+    print(save_model_path)
 
-N_train = 50
-model_name =  f"BayesianNN_multivariate_2layer_N{N_train}"      # Choose LinearRegression or NN 
+    # Get data
+    X = np.load(f'{data_path}/X_train_dtf.npy')
+    U = np.load(f'{data_path}/U_train_dtf.npy')
+    print(f'Data loaded from {data_path}')
 
-# Define model and guide
-model = BayesianNN(1, 1, [32, 32])
-total_params = sum(p.numel() for p in model.parameters())
+    # Subsample to remove correlations
+    subsample = 1000 # (1 Time Units)
+    X = X[::subsample]
+    U = U[::subsample]
 
-print("TOTAL PARAMS: ", total_params)
+    N = X.shape[0]
+    N_train = N_train #int(0.60 * N)
+    N_val = max(N - N_train, 0)   # Use remainder for validation
 
-# Guide
-#guide = AutoDiagonalNormal(model)
-guide = AutoLowRankMultivariateNormal(model)
+    features = np.ravel(X[:N_train])   
+    targets = np.ravel(U[:N_train])   
 
-# Set up directory
-data_path = f'./data/K{K}_J{J}_h{h}_c{c}_b{b}_F{F}'
-save_model_path = f'{data_path}/{model_name}/'
-if not os.path.exists(save_model_path):
-    os.makedirs(save_model_path)
-print(save_model_path)
+    features_val = np.ravel(X[N_train:N_train+N_val])   
+    targets_val = np.ravel(U[N_train:N_train+N_val])    
 
-# Get data
-X = np.load(f'{data_path}/X_train_dtf.npy')
-U = np.load(f'{data_path}/U_train_dtf.npy')
-print(f'Data loaded from {data_path}')
+    print(features.shape, targets.shape, features_val.shape, targets_val.shape)
 
-# Subsample to remove correlations
-subsample = 1000 # (1 Time Units)
-X = X[::subsample]
-U = U[::subsample]
+    X_torch = torch.tensor(features, dtype=torch.float32).reshape((-1, 1))
+    Y_torch = torch.tensor(targets, dtype=torch.float32).reshape((-1, 1))
 
-N = X.shape[0]
-N_train = N_train #int(0.60 * N)
-N_val = max(N - N_train, 0)   # Use remainder for validation
+    X_val = torch.tensor(features_val, dtype=torch.float32).reshape((-1, 1))
+    Y_val = torch.tensor(targets_val, dtype=torch.float32).reshape((-1, 1))
 
-features = np.ravel(X[:N_train])   
-targets = np.ravel(U[:N_train])   
+    # Optimisation settings
+    adam = pyro.optim.Adam({"lr": 0.003})
+    svi = SVI(model, guide, adam, loss=Trace_ELBO())
 
-features_val = np.ravel(X[N_train:N_train+N_val])   
-targets_val = np.ravel(U[N_train:N_train+N_val])    
+    num_iterations=5000
+    losses = []
+    losses_val = []
+    min_loss = 1E8
 
-print(features.shape, targets.shape, features_val.shape, targets_val.shape)
+    pyro.clear_param_store()
+    num_iterations = 6000 
 
-X_torch = torch.tensor(features, dtype=torch.float32).reshape((-1, 1))
-Y_torch = torch.tensor(targets, dtype=torch.float32).reshape((-1, 1))
+    for iteration in range(num_iterations):
+        # calculate the loss and take a gradient step
+        loss = svi.step(X_torch, Y_torch)
+        losses.append(loss)
+        if iteration % 100 == 0:
+            print("[iteration %04d] loss: %.4f" % (iteration + 1, loss ))
 
-X_val = torch.tensor(features_val, dtype=torch.float32).reshape((-1, 1))
-Y_val = torch.tensor(targets_val, dtype=torch.float32).reshape((-1, 1))
+        if loss < min_loss:
+            # Save checkpoint
+            output_dicts = {
+                "iteration": iteration,
+                "train_loss": losses[-1],
+                "model": model,
+                "guide":guide}
 
-# Optimisation settings
-adam = pyro.optim.Adam({"lr": 0.03})
-svi = SVI(model, guide, adam, loss=Trace_ELBO())
+            torch.save(output_dicts, f"{save_model_path}/model_best.pt")
+            pyro.get_param_store().save( f"{save_model_path}/pyro_best_params.pt")
+            min_loss = loss
 
-num_iterations=5000
-losses = []
-losses_val = []
-min_loss = 1E8
+    print("Done training")
 
-pyro.clear_param_store()
-num_iterations = 1500 
+    # Save results
+    output_dicts = {
+        "iteration": iteration,
+        "train_loss": losses[-1],
+        "np_rng_state": np.random.get_state(),
+        "torch_rng_state": torch.random.get_rng_state(),
+        "model":model,
+        "guide":guide}
 
-for iteration in range(num_iterations):
-    # calculate the loss and take a gradient step
-    loss = svi.step(X_torch, Y_torch)
-    losses.append(loss)
-    if iteration % 100 == 0:
-        print("[iteration %04d] loss: %.4f" % (iteration + 1, loss ))
-
-    if loss < min_loss:
-        # Save checkpoint
-        output_dicts = {
-            "iteration": iteration,
-            "train_loss": losses[-1],
-            "model": model,
-            "guide":guide}
-
-        torch.save(output_dicts, f"{save_model_path}/model_best.pt")
-        pyro.get_param_store().save( f"{save_model_path}/pyro_best_params.pt")
-        min_loss = loss
-
-print("Done training")
-
-# Save results
-output_dicts = {
-    "iteration": iteration,
-    "train_loss": losses[-1],
-    "np_rng_state": np.random.get_state(),
-    "torch_rng_state": torch.random.get_rng_state(),
-    "model":model}
-
-torch.save(output_dicts, f"{save_model_path}/model.pt")
-pyro.get_param_store().save( f"{save_model_path}/pyro_params.pt")
-print("Model saved to ", save_model_path)
+    torch.save(output_dicts, f"{save_model_path}/model.pt")
+    pyro.get_param_store().save( f"{save_model_path}/pyro_params.pt")
+    print("Model saved to ", save_model_path)
 
 
-# Plot and save losses
-plt.clf()
-figure, ax = plt.subplots(1)
-plt.semilogy(losses)
-#plt.semilogy(losses_val, alpha=0.5)
-plt.xlabel("Iterations")
-plt.ylabel("Loss")
-plt.savefig(f"{save_model_path}/losses.png")
+    # Plot and save losses
+    plt.clf()
+    figure, ax = plt.subplots(1)
+    plt.semilogy(losses)
+    #plt.semilogy(losses_val, alpha=0.5)
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.savefig(f"{save_model_path}/losses.png")
+    print(f"{save_model_path}/losses.png")
 
-# Plot and save result
-plt.clf()
-figure, ax = plt.subplots(1)
-X_domain = torch.linspace(-15, 20., 100).unsqueeze(-1)
-pred = model(X_domain).detach()
+    # Plot and save result
+    plt.clf()
+    figure, ax = plt.subplots(1)
+    X_domain = torch.linspace(-15, 20., 100).unsqueeze(-1)
+    pred = model(X_domain).detach()
 
-# Plot
-plt.scatter(X_torch.flatten()[::], Y_torch.flatten()[::], color="k", alpha=0.2)
-plt.axis(ymin=-15., ymax=20.,xmin=-15., xmax=20.)
-plt.xticks(fontsize=18)
-plt.yticks(fontsize=18)
-plt.xlabel("$X$", fontsize=18)
-plt.ylabel("$U$", fontsize=18)
-plt.tight_layout()
-plt.savefig(f"{save_model_path}/data.png")
+    # Plot
+    plt.scatter(X_torch.flatten()[::], Y_torch.flatten()[::], color="k", alpha=0.2)
+    plt.axis(ymin=-15., ymax=20.,xmin=-15., xmax=20.)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.xlabel("$X$", fontsize=18)
+    plt.ylabel("$U$", fontsize=18)
+    plt.tight_layout()
+    plt.savefig(f"{save_model_path}/data.png")
 
 
-predictive = Predictive(model, guide=guide, num_samples=800,
-                        return_sites=("obs", "_RETURN", "sigma"))
-samples = predictive(X_domain)
-pred_summary = summary_stats(samples)
+    predictive = Predictive(model, guide=guide, num_samples=800,
+                            return_sites=("obs", "_RETURN", "sigma"))
+    samples = predictive(X_domain)
+    pred_summary = summary_stats(samples)
 
-plt.plot(X_domain.squeeze(), pred_summary["_RETURN"]["mean"].squeeze(), color="r", linewidth=2)
+    plt.plot(X_domain.squeeze(), pred_summary["_RETURN"]["mean"].squeeze(), color="r", linewidth=2)
 
-plt.fill_between(X_domain.squeeze(), 
-                 pred_summary["obs"]["5%"].squeeze(), 
-                 pred_summary["obs"]["95%"].squeeze(),
-                 color="green", alpha=0.2)
+    plt.fill_between(X_domain.squeeze(), 
+                    pred_summary["obs"]["5%"].squeeze(), 
+                    pred_summary["obs"]["95%"].squeeze(),
+                    color="green", alpha=0.2)
 
-plt.fill_between(X_domain.squeeze(), 
-                 pred_summary["_RETURN"]["5%"].squeeze(), 
-                 pred_summary["_RETURN"]["95%"].squeeze(),
-                 color="purple", alpha=0.2)
+    plt.fill_between(X_domain.squeeze(), 
+                    pred_summary["_RETURN"]["5%"].squeeze(), 
+                    pred_summary["_RETURN"]["95%"].squeeze(),
+                    color="purple", alpha=0.2)
 
-plt.xlabel("Parameterisation input")
-plt.ylabel("Parameterisation output")
-plt.title("2-layer NN")
+    plt.xlabel("Parameterisation input")
+    plt.ylabel("Parameterisation output")
+    plt.title("2-layer NN")
 
-plt.savefig(f"{save_model_path}/input_outputs_NN.png")
-print("Plots done")
+    plt.savefig(f"{save_model_path}/input_outputs_NN.png")
+    print(f"{save_model_path}/input_outputs_NN.png")
+
+    print("Plots done")
+
+
+if __name__ == "__main__":
+    params ={
+        'F': 20,
+        'c': 10,
+        'b': 10,
+        'h': 1,
+        'J': 32,
+        'K': 8,
+        'dt': 0.001,
+        'dt_f': 0.005,
+    }
+    training_params = {'N_train': 50, 
+                       'batch_size':128,
+                       'N_timesteps':1}
+    N_train = training_params['N_train']
+    seed = 123
+    np.random.seed(seed)
+
+    model_name =  f"BayesianNN_multivariatefull_16_N{N_train}"      # Choose LinearRegression or NN 
+
+    # Define model and guide
+    model = BayesianNN(1, 1, [16, 16])
+        
+
+    # Guide
+    #guide = AutoDiagonalNormal(model)
+    guide = AutoLowRankMultivariateNormal(model)
+    guide = AutoMultivariateNormal(model)
+
+    bayesian_train(params, training_params, model_name, model, guide)
 

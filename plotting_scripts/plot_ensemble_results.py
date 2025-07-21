c6c2ce5df42af2138a97d6b2f31f6ed2991c69f8
lm2612 <lauraman@stanford.edu>
2025-07-21 11:55:10 +0100

    made function

--- plotting_scripts/plot_ensemble_results.py
+++ plotting_scripts/plot_ensemble_results.py
@@ -8,64 +8,68 @@ from ml_models.TorchModels import LinearRegression, NN
 from L96.L96_model import L96OneLayerParam
 from utils.kde_plot import kde_plot
 
-from plot_dicts import colors, labels, plotcolor
+from plot_dicts import plotcolor
 
 
-# Get paths
-# Define dimensions of system (fixed)
-K = 8   
-J = 32  
+def plot_distributions(params, model_name, run_types, label_names, save_prefix=""):
+    """Plots distributions """
+    K, J, h, F, c, b = params['K'], params['J'], params['h'], params['F'], params['c'], params['b']
+    dt, dt_f = params['dt'], params['dt_f']
 
-# Define the "true" parameters
-h = 1
-F = 20  
-c = 10
-b = 10
+    # Set up directories
+    data_path = f'./data/K{K}_J{J}_h{h}_c{c}_b{b}_F{F}'
+    model_path = f'{data_path}/{model_name}/'
+    filenames = [f'{model_path}/{run_type}_X_dtf.npy' for run_type in run_types]
+    print(filenames)
 
-# Define time-stepping, random seed
-dt = 0.001
-dt_f = dt * 5
-seed = 123
-np.random.seed(seed)
+    plot_path = f'{model_path}/plots/'
+    if not os.path.exists(plot_path):
+        os.makedirs(plot_path)
 
 
-# Set up model and types of simulations to plot
-N_train = 50
-model_name = f"BayesianNN_16_N{N_train}"
-run_types = ["epistemic", "aleatoric", "both"] # Or run_types = ["epistemic_fix", "aleatoric_AR1_", ...]
-colors = ["darkorchid", "seagreen", "dimgrey"]
-label_names = [ "Epistemic", "Aleatoric", "Both"]
-save_prefix = "whitenoise_"
+
+    # Load truth data
+    X_truth = np.load(f"{data_path}/X_dtf.npy")
+
+    # Load ml param model results
+    test_params = [np.load(f"{model_path}/{run_type}_test_params.npy", allow_pickle=True).item() for run_type in run_types]
+    X_mls = [np.load(filename) for filename in filenames]
+
+    # Plot distributions
+    fig = plt.figure(figsize=(10, 5))
+    X_domain = np.linspace(-25., 25., 80)
+    pdf_truth = kde_plot(X_truth[:], X_domain)
+    plt.plot(pdf_truth, color="black", label="Truth")
+    for X_ml, run_type, label_name in zip(X_mls, run_types, label_names):
+        pdf = kde_plot(X_ml[:], X_domain)
+        plt.plot(pdf, 
+                label=label_name,
+                color=plotcolor(run_type),
+                alpha=0.6)
+    plt.legend()
+    plt.savefig(f"{plot_path}{save_prefix}X_pdf.png")
+    print(f"Saved to {plot_path}{save_prefix}X_pdf.png")
 
 
-# Set up directories
-data_path = f'./data/K{K}_J{J}_h{h}_c{c}_b{b}_F{F}'
-model_path = f'{data_path}/{model_name}/'
-filenames = [f'{model_path}/{run_type}_X_dtf.npy' for run_type in run_types]
-print(filenames)
+if __name__ == "__main__":
+    params ={
+        'F': 20,
+        'c': 10,
+        'b': 10,
+        'h': 1,
+        'J': 32,
+        'K': 8,
+        'dt': 0.001,
+        'dt_f': 0.005,
+    }
 
-plot_path = f'{model_path}/plots/'
-if not os.path.exists(plot_path):
-    os.makedirs(plot_path)
+    # Set up model and types of simulations to plot
+    N_train = 50
+    model_name = f"BayesianNN_16_N{N_train}"
+    run_types = ["epistemic", "aleatoric", "both"] # Or run_types = ["epistemic_fix", "aleatoric_AR1_", ...]
+    label_names = [ "Epistemic", "Aleatoric", "Both"]
+    save_prefix = "whitenoise_"
 
-# Load truth data
-X_truth = np.load(f"{data_path}/X_dtf.npy")
+    plot_distributions(params, model_name, run_types, label_names, save_prefix=save_prefix)
 
-# Load ml param model results
-test_params = [np.load(f"{model_path}/{run_type}_test_params.npy", allow_pickle=True).item() for run_type in run_types]
-X_mls = [np.load(filename) for filename in filenames]
 
-# Plot distributions
-fig = plt.figure(figsize=(10, 5))
-X_domain = np.linspace(-25., 25., 80)
-pdf_truth = kde_plot(X_truth[:], X_domain)
-plt.plot(pdf_truth, color="black", label="Truth")
-for X_ml, run_type, label_name in zip(X_mls, run_types, label_names):
-    pdf = kde_plot(X_ml[:], X_domain)
-    plt.plot(pdf, 
-            label=label_name,
-            color=plotcolor(run_type),
-            alpha=0.6)
-plt.legend()
-plt.savefig(f"{plot_path}{save_prefix}X_pdf.png")
-print(f"Saved to {plot_path}{save_prefix}X_pdf.png")

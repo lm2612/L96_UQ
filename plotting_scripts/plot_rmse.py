219df9c46a59988e14690bdc7d12cd7dc239ecf3
lm2612 <lauraman@stanford.edu>
2025-07-30 15:51:22 +0100

    added days axis, added spread plot, streamlined calculations into arrays to repeatedly use variance

--- plotting_scripts/plot_rmse.py
+++ plotting_scripts/plot_rmse.py
@@ -10,8 +10,9 @@ from utils.kde_plot import kde_plot
 from utils.crps import crps
 
 from plotting_scripts.plot_dicts import plotcolor
+from utils.add_time_axis import add_axis_weather
 
-def plot_error_trajectories(params, model_name, run_types, label_names, save_prefix="", plot_spread=True):
+def plot_error_trajectories(params, model_name, run_types, label_names, save_prefix="", plot_spread=True, include_sum=False):
     """Plots error trajectories """
     K, J, h, F, c, b = params['K'], params['J'], params['h'], params['F'], params['c'], params['b']
     dt, dt_f = params['dt'], params['dt_f']
@@ -29,55 +30,86 @@ def plot_error_trajectories(params, model_name, run_types, label_names, save_pre
     # Load truth data
     X_truth = np.load(f"{data_path}/X_dtf.npy")
 
-    # Load ml param model results
-    X_mls = [np.load(filename) for filename in filenames]
-
-    # For all plots, time separation assumed to be 10
+    # Load ml param model results - must all be same size
+    X_mls = np.stack([np.load(filename) for filename in filenames])
+    n_ens = X_mls.shape[1]
+    # Reshape for initial conditions
     T = 10
     nt = int(T/dt_f)
-    N_init = X_mls[0].shape[1] // nt
+    N_init = X_mls.shape[2] // nt
     time = np.arange(0, T, dt_f)
-
-    print(f"{N_init} initial conditions separated by {nt} time units")
-    X_init_conds = X_truth[0:N_init:nt]
-
-    # Reshape
+    X_mls = X_mls.reshape((len(filenames), n_ens, N_init, nt, K))
+    # truth
     N_init_truth = X_truth.shape[0] // nt 
     X_truth = X_truth.reshape(((N_init_truth, nt, K)))[:N_init]
 
+    # Compute mean and var across ensemble (axis=1)
+    X_mean = X_mls.mean(axis=1)
+    X_var = X_mls.var(axis=1)
+
+    # Take mean variance across all initial conditions  (axis=1) and dims K (axis=3)
+    print(X_mls.shape, X_mean.shape)
+    X_var_m = X_var.mean(axis=(1,3))
+
+    # Calc RMSE
+    X_rmse = np.sqrt((X_mean - X_truth)**2).mean(axis=(1,3))
+    print(X_var_m.shape, X_rmse.shape)
+
     # Plot error trajectories
     fig, axs = plt.subplots(1, 1, figsize=(6, 4))
-    for X_ml, run_type, label_name in zip(X_mls, run_types, label_names):
-        # Take mean prediction across ensembles (only relevant if n_ens > 1)
-        X_m = X_ml.mean(axis=0)
-        #N_init = X_ml.shape[0] // nt
-        X_m = X_m.reshape((N_init, nt, K))
-        # Take rmse across all initial conditions (axis=0) and variables (axis=2)
-        X_diff = np.sqrt(((X_m - X_truth)**2).mean(axis=(0, 2)))
-        axs.plot(time[0:nt], X_diff, 
-            label=label_name, 
+    for r in range(len(run_types)):
+        axs.plot(time[0:nt], X_rmse[r], 
+            label=label_names[r], 
             alpha=0.8,
-            color=plotcolor(run_type))
+            color=plotcolor(run_types[r]))
 
         if plot_spread:
-            # Get spread in err across ensembles
-            X_var = X_ml.var(axis=0)
-            X_var = X_var.reshape((N_init, nt, K))
-            # Take mean across all initial conditions (axis=0) and variables (axis=2)
-            X_std = np.sqrt(X_var.mean(axis=(0, 2)))
-
-            axs.plot(time[0:nt], X_std, 
+            axs.plot(time[0:nt], np.sqrt(X_var_m[r]), 
                 alpha=0.8,
-                color=plotcolor(run_type),
+                color=plotcolor(run_types[r]),
                 linestyle="dashed")
 
-    axs.axis(xmin=0, xmax=10)
+    axs.axis(xmin=0, xmax=6)
     axs.legend(loc="lower right")
     axs.set_ylabel(f"X")
     axs.set_xlabel("Time")
+    add_axis_weather(axs,  max_days = 35., step_days = 10.)
+
     plt.tight_layout()
     plt.savefig(f"{plot_path}/X_rmse_timeseries.png")
     print(f"Saved to {plot_path}/X_rmse_timeseries.png")
+
+
+    # Plot variance only
+    variance_sum = 0
+    plt.clf()
+    fig, axs = plt.subplots(1, 1, figsize=(6, 4))
+
+    for r in range(len(run_types)):
+        axs.plot(time[0:nt], np.sqrt(X_var_m[r]), 
+            alpha=0.8,
+            color=plotcolor(run_types[r]),
+            label=label_names[r])
+        if include_sum and (("epistemic" or "aleatoric") in run_types[r]):
+            variance_sum += X_var_m[r]
+        
+    # Include sum of epistemic and aleatoric
+    if include_sum:
+        axs.plot(time[0:nt], np.sqrt(variance_sum), 
+                alpha=0.8,
+                color="gray",
+                label="sum",
+                linestyle="dashed")
+
+    axs.axis(xmin=0, xmax=6)
+    axs.legend(loc="lower right")
+    axs.set_ylabel(f"X")
+    axs.set_xlabel("Time")
+    add_axis_weather(axs,  max_days = 35., step_days = 10.)
+
+    plt.tight_layout()
+    plt.savefig(f"{plot_path}/X_std_timeseries.png")
+    print(f"Saved to {plot_path}/X_std_timeseries.png")
 
     # Plot CRPS
     plt.clf()
@@ -92,10 +124,11 @@ def plot_error_trajectories(params, model_name, run_types, label_names, save_pre
                 label=label_name, 
                 alpha=0.8,
                 color=plotcolor(run_type))
-    axs.axis(xmin=0, xmax=4)
+    axs.axis(xmin=0, xmax=6)
     axs.legend(loc="lower right")
     axs.set_ylabel(f"X")
     axs.set_xlabel("Time")
+    add_axis_weather(axs,  max_days = 35., step_days = 10.)
     plt.tight_layout()
     plt.savefig(f"{plot_path}/X_crps_timeseries.png")
 

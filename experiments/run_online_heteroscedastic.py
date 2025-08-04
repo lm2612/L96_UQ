041b14204b16ff023e05ae79dba913c5d8a3548a
lm2612 <lauraman@stanford.edu>
2025-08-04 12:11:40 +0100

    update both - was accidently returning mean rather than out which includes aleatoric

--- experiments/run_online_heteroscedastic.py
+++ experiments/run_online_heteroscedastic.py
@@ -27,14 +27,14 @@ test_params = { 'fname':'X_dtf.npy',
                 'runtype': None,
                 'save_model_path':'',
                 'save_prefix':'',
-                'n_ens': 1,
-                'N_init': 1,
+                'n_ens': 50,
+                'N_init': 100,
                 'save_step': 1,
                 'T':10 ,
                 'F':20                  }
 
 # Model name
-model_name =  f"BayesianNN_Heteroscedastic_16_N50" 
+model_name =  f"BayesianNN_Heteroscedastic_16_16_N100" 
 model_path = f"./data/K{params['K']}_J{params['J']}_h{params['h']}_c{params['c']}_b{params['b']}_F{params['F']}/{model_name}/"
 test_params['save_model_path'] = model_path
 
@@ -43,12 +43,16 @@ output_dicts = torch.load(f"{model_path}/model_best.pt", weights_only=False)
 pyro.get_param_store().load(f"{model_path}/pyro_params.pt")
 pyro_model = output_dicts["model"]
 guide = output_dicts["guide"]
-predictive = Predictive(pyro_model, guide=guide, num_samples=1, return_sites=("_RETURN", "obs"))
 
 # Run Epistemic with white noise
 def param_func(x):
-    out = predictive(x.unsqueeze(-1))["_RETURN"]
-    return out.squeeze()
+    fixed_param_NN = pyro_model.get_fixed_param_NN(guide())
+    fixed_param_NN.eval()
+    with torch.no_grad():
+        mean_sigma = fixed_param_NN(x.unsqueeze(-1))
+    mean, sigma = mean_sigma.chunk(2, dim=-1)
+    return mean.squeeze()
+
 test_params['runtype'] = 'epistemic'
 test_params['save_prefix'] = 'epistemic_' 
 test(params, test_params, param_func)
@@ -58,8 +62,8 @@ fixed_param_NN = pyro_model.get_fixed_param_NN(guide.median())
 fixed_param_NN.eval()
 def param_func(x):
     with torch.no_grad():
-        mean = fixed_param_NN(x.unsqueeze(-1))
-        out = pyro_model.sample_obs(mean)
+        mean_sigma = fixed_param_NN(x.unsqueeze(-1))
+        out = pyro_model.sample_obs(mean_sigma)
     return out.squeeze()
 
 test_params['runtype'] = 'aleatoric'
@@ -68,7 +72,11 @@ test(params, test_params, param_func)
 
 # Run both types of uncertainty 
 def param_func(x):
-    out = predictive(x.unsqueeze(-1))["obs"]
+    fixed_param_NN = pyro_model.get_fixed_param_NN(guide())
+    fixed_param_NN.eval()
+    with torch.no_grad():
+        mean_sigma = fixed_param_NN(x.unsqueeze(-1))
+        out = pyro_model.sample_obs(mean_sigma)
     return out.squeeze()
 
 test_params['runtype'] = 'both'

077eb3b769eedfd1f080fc25b3c43ea85ff129fb
lm2612 <lauraman@stanford.edu>
2025-10-15 15:36:13 +0100

    run and save F=16, F=24 more general

--- experiments/run_truth_climate_simulations.py
+++ experiments/run_truth_climate_simulations.py
@@ -23,22 +23,23 @@ params ={
     'dt_f': 0.005,
 }
 
-test_params = { 'save_path':f"./data/K{params['K']}_J{params['J']}_h{params['h']}_c{params['c']}_b{params['b']}_F{params['F']}/",
-                'load_prefix': '',
-                'save_prefix': 'run00_', 
-                'save_Y': False,
-                'save_ICs': True,
-                'T':1000 ,
-                'F':20                  }
+for F_test in [20, 16, 24]:
+    test_params = { 'save_path':f"./data/K{params['K']}_J{params['J']}_h{params['h']}_c{params['c']}_b{params['b']}_F{params['F']}/",
+                    'load_prefix': '',
+                    'save_prefix': f'climate_F{F_test}_run00_', 
+                    'save_Y': False,
+                    'save_ICs': True,
+                    'T':1000 ,
+                    'F':F_test                  }
 
 
-seed = 123
-np.random.seed(seed)
-generate_truth(params, test_params)
-
-for i in range(0, 10):
-    test_params['load_prefix'] = f'run{i:02d}_'
-    test_params['save_prefix'] = f'run{i+1:02d}_'
-    print(f"Loading from {test_params['load_prefix']}, saving to {test_params['save_prefix']}")
-
+    seed = 123
+    np.random.seed(seed)
     generate_truth(params, test_params)
+
+    for i in range(0, 10):
+        test_params['load_prefix'] = f'run{i:02d}_'
+        test_params['save_prefix'] = f'climate_F{test_params["F"]}_run{i+1:02d}_'
+        print(f"Loading from {test_params['load_prefix']}, saving to {test_params['save_prefix']}")
+
+        generate_truth(params, test_params)

ef5405eaa440e648f13c2145db12445aab1e8722
lm2612 <lauraman@stanford.edu>
2025-11-27 23:09:07 +0000

    fig. 7

--- experiments/plot_skill_spread_metrics.py
+++ experiments/plot_skill_spread_metrics.py
@@ -5,7 +5,6 @@ import torch
 
 from plotting_scripts.plot_skill_spread_metrics import plot_spread_v_skill
 
-
 # Set up parameters for simulation
 params ={
     'F': 20,
@@ -20,14 +19,17 @@ params ={
 
 # Set up model and types of simulations to plot
 model_name = f"BayesianNN_Heteroscedastic_16_16_N100"
-run_types = ["aleatoric", "epistemic", "both"] #, "both_AR1" ] #, "IC_aleatoric", "aleatoric"] #, "aleatoric", "both"] #, "aleatoric_AR1", "both_fix_AR1"] # Or run_types = ["epistemic_fix", "aleatoric_AR1_", ...]
-label_names = ["Aleatoric", "Epistemic", "Both"]#, "Both"] #, "Aleatoric+InitCond", "Aleatoric"] #"Epistemic", "Aleatoric", "Both"]
-save_prefix = "WN_"
+run_types = ["aleatoric", "epistemic", "both"] 
+label_names = ["Aleatoric (indep.)", "Epistemic (indep.)", "Both (indep.)"]
+save_prefix = "Indep_"
 
-plot_spread_v_skill(params, model_name, run_types, label_names, save_prefix=save_prefix)
+plot_spread_v_skill(params, model_name, run_types, label_names, save_prefix=save_prefix, 
+    ylim=2., samples_per_bin=100)
 
-run_types = ["aleatoric_AR1", "epistemic_AR1", "both_AR1"] #, "both_AR1" ] #, "IC_aleatoric", "aleatoric"] #, "aleatoric", "both"] #, "aleatoric_AR1", "both_fix_AR1"] # Or run_types = ["epistemic_fix", "aleatoric_AR1_", ...]
-label_names = ["Aleatoric (AR1)", "Epistemic (AR1)", "Both (AR1)"]#, "Both"] #, "Aleatoric+InitCond", "Aleatoric"] #"Epistemic", "Aleatoric", "Both"]
+run_types = ["aleatoric_AR1", "new_epistemic_AR1", "new_both_AR1"] 
+label_names = ["Aleatoric (AR1)", "Epistemic (AR1)", "Both (AR1)"]
 save_prefix = "AR1_"
 
-plot_spread_v_skill(params, model_name, run_types, label_names, save_prefix=save_prefix)
+plot_spread_v_skill(params, model_name, run_types, label_names, save_prefix=save_prefix, 
+    ylim=2., samples_per_bin=100)
+

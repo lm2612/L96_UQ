7a373fdbcd5076b002d5b8a155fea7aa9dd9b2ba
lm2612 <lauraman@stanford.edu>
2025-11-27 22:37:33 +0000

    add option to plot spatialmean (doesnt change dists much)

--- plotting_scripts/plot_distributions.py
+++ plotting_scripts/plot_distributions.py
@@ -12,7 +12,7 @@ from plotting_scripts.plot_dicts import plotcolor
 
 
 def plot_distributions(params, model_name, run_types, label_names, fnames="X_dtf",
-    fnames_true=None, det=False, save_prefix="", linestyles = None):
+    fnames_true=None, det=False, save_prefix="", linestyles = None, spatial_mean=False):
     """Plots distributions """
     K, J, h, F, c, b = params['K'], params['J'], params['h'], params['F'], params['c'], params['b']
     dt, dt_f = params['dt'], params['dt_f']
@@ -37,15 +37,24 @@ def plot_distributions(params, model_name, run_types, label_names, fnames="X_dtf
     print([f'{data_path}/{fname}.npy' for fname in fnames_true])
     X_truth = np.stack([np.load(f'{data_path}/{fname}.npy') for fname in fnames_true])
     print(X_truth.shape)
-    
+    if spatial_mean:
+        X_truth = X_truth.mean(axis=-1)
+        print(X_truth.shape)
+
     # Load ml param model results - must all be same size
     print([[(f'{model_path}/{run_type}_{fname}.npy') for fname in fnames] for run_type in run_types])
     X_mls = np.stack([[np.load(f'{model_path}/{run_type}_{fname}.npy') for fname in fnames] for run_type in run_types])
     print(X_mls.shape)
+    if spatial_mean:
+        X_mls = X_mls.mean(axis=-1)
+        print(X_mls.shape)
 
     # Load det if its provided
     if det:
         X_det = np.stack([np.load(f'{model_path}/deterministic_{fname}.npy') for fname in fnames])
+        if spatial_mean:
+            X_det = X_det.mean(axis=-1)
+            print(X_det.shape)
 
     # Plot distributions
     fig, axs = plt.subplots(2, 1, figsize=(8, 6), dpi=100, sharex=True)
@@ -90,8 +99,12 @@ def plot_distributions(params, model_name, run_types, label_names, fnames="X_dtf
 
     #plt.legend()
     plt.tight_layout()
-    plt.savefig(f"{plot_path}{save_prefix}X_pdf_and_diff.png")
-    print(f"Saved to {plot_path}{save_prefix}X_pdf_and_diff.png")
+    if spatial_mean:
+        ext = "_spatial_mean"
+    else:
+        ext = ""
+    plt.savefig(f"{plot_path}{save_prefix}X_pdf_and_diff{ext}.png")
+    print(f"Saved to {plot_path}{save_prefix}X_pdf_and_diff{ext}.png")
 
 
 if __name__ == "__main__":
@@ -117,7 +130,7 @@ if __name__ == "__main__":
 
 
     N_train = 100
-    F_test = 14
+    F_test = 20
     model_name = f"BayesianNN_16_16_N{N_train}"
     run_types = ["epistemic_fix", "aleatoric_AR1", "both_fix_AR1", "epistemic_fix"] #, "aleatoric",] # Or run_types = ["epistemic_fix", "aleatoric_AR1_", ...]
     label_names = [ "Epistemic (fix)", "Aleatoric (AR1)", "Both", "Epistemic (PPE)"]

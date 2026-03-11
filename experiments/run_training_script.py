c85ef86f3a51ed2f580101b74f6ef990680e8cfc
lm2612 <lauraman@stanford.edu>
2026-03-11 12:15:01 +0000

    save prefix added

--- experiments/run_training_script.py
+++ experiments/run_training_script.py
@@ -19,13 +19,17 @@ params ={
     }
 training_params = {'N_train': 100, 
                     'batch_size':128,
-                    'N_timesteps':1}
+                    'N_timesteps':1,
+                    'predict_sigma':False,
+                    'save_prefix':''}
 N_train = training_params['N_train']
-seeds = range(100, 101) #, 150)
+seeds = range(100, 150)
 for seed in seeds:
-    model_name =  f"NN_N{N_train}"      # Choose LinearRegression or NN 
+    training_params['save_prefix'] = f"seed{seed}"
     np.random.seed(seed)
-    model = NN(1, 1) #, dropout_rate=0.5)
+
+    model_name =  f"NN_N{N_train}"      # Choose LinearRegression or NN 
+    model = NN(1, 1, [16, 16])  
     total_params = sum(p.numel() for p in model.parameters())
     print("TOTAL PARAMS: ", total_params)
     train(params, training_params, model_name, model)
